5c51b3ece2198827deb663ee248961e88f991a0c
Janko Simonovic <simonovic86@gmail.com>
2020-05-28 11:57:38 +0200

    Code refactor (#6)
    - introduce eslint
    - fix npm start

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    "no-underscore-dangle": 'off',
+    "no-await-in-loop": 'off',
+  },
+};
